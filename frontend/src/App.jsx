d5e5e23e0881743ce20c0f3dba3f72f2b954d5ae
kshitijkhowal <kshitij.durgesh@gmail.com>
2025-01-28 21:26:57 +0530

    vite and tailwind setup and starting files created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import { Router,Routes,Navigate,Route } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
+import ProductList from './pages/ProductList';
+import ProductForm from './pages/ProductForm';
+import ProductDetails from './pages/ProductDetails';
+import Navbar from './components/Navbar';
+import HomePage from './pages/HomePage';
 
-function App() {
-  const [count, setCount] = useState(0)
 
+
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/" element={<ProductList />} />
+        <Route path="/product/new" element={<ProductForm />} />
+        <Route path="/product/:id" element={<ProductDetails />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </div>
   )
 }
 
